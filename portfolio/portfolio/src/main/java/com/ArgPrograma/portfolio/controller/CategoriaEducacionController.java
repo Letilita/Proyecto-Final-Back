4e2af3ea9572d5eb2d9e9548afd6b649f79ae52f
Letilita <leticia.portillo.r@gmail.com>
2022-07-31 15:43:50 -0300

    Get desde Back-End completo

--- portfolio/portfolio/src/main/java/com/ArgPrograma/portfolio/controller/CategoriaEducacionController.java
+++ portfolio/portfolio/src/main/java/com/ArgPrograma/portfolio/controller/CategoriaEducacionController.java
@@ -5,6 +5,7 @@ import com.ArgPrograma.portfolio.model.CategoriaEducacion;
 import com.ArgPrograma.portfolio.service.ICategoriaEducacionService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/CategoriasEducacion")
+@CrossOrigin(origins= "http://localhost:4200")
 
 public class CategoriaEducacionController {
     @Autowired
